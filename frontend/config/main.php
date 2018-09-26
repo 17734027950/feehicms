2526c9b295dddf7389c3c390bc4b8e488ec9c29d
17734027950 <17734027950@qq.com>
2018-09-26 15:35:01 +0800

    '1.0.7'

--- frontend/config/main.php
+++ frontend/config/main.php
@@ -50,7 +50,7 @@ return [
             'enablePrettyUrl' => true,
             'showScriptName' => false,//隐藏index.php
             'enableStrictParsing' => false,
-            //'suffix' => '.html',//后缀，如果设置了此项，那么浏览器地址栏就必须带上.html后缀，否则会报404错误
+            'suffix' => '.shtml',//后缀，如果设置了此项，那么浏览器地址栏就必须带上.html后缀，否则会报404错误
             'rules' => [
                 //'<controller:\w+>/<action:\w+>'=>'<controller>/<action>',
                 //'<controller:\w+>/<action:\w+>/<id:\d+>'=>'<controller>/<action>?id=<id>'
